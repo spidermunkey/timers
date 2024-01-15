f7fae22ada749a2a95b14b9df86ab762264a068c
spidermunkey <burkstremane@gmail.com>
2024-01-14 20:13:21 -0700

    add play/pause icon/styles

--- latest/public/javascript/components/Timer.js
+++ latest/public/javascript/components/Timer.js
@@ -333,10 +333,15 @@ export class Timer {
                 <div class="timer--clock-controls">
                     <div class="ctrl-wrapper">
                         <div class="play ctrl current">
-                            <span class="control">play</span>
+                            <span class="control"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                            <!--! Font Awesome Free 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2022 Fonticons, Inc. --><path d="M361 215C375.3 223.8 384 239.3 384 256C384 272.7 375.3 288.2 361 296.1L73.03 472.1C58.21 482 39.66 482.4 24.52 473.9C9.377 465.4 0 449.4 0 432V80C0 62.64 9.377 46.63 24.52 38.13C39.66 29.64 58.21 29.99 73.03 39.04L361 215z"></path></svg></span>
                         </div>
                         <div class="pause ctrl">
-                            <span class="control">pause</span>
+                            <span class="control">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                            <!--! Font Awesome Free 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2022 Fonticons, Inc. --><path d="M272 63.1l-32 0c-26.51 0-48 21.49-48 47.1v288c0 26.51 21.49 48 48 48L272 448c26.51 0 48-21.49 48-48v-288C320 85.49 298.5 63.1 272 63.1zM80 63.1l-32 0c-26.51 0-48 21.49-48 48v288C0 426.5 21.49 448 48 448l32 0c26.51 0 48-21.49 48-48v-288C128 85.49 106.5 63.1 80 63.1z"></path>
+                                </svg>
+                            </span>
                         </div>
                     </div>
                     <div class="reset">reset</div>
@@ -576,7 +581,7 @@ export class TimeTracker extends Timer {
         }
 
         const success = await api.log(log)
-        
+
         if (success)
             console.log(success)
         else 
