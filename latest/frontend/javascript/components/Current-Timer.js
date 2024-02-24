f958de0b7b6fb1ecf488f7ef99958ec583607f35
spider <burkstremane@gmail.com>
2024-02-24 11:52:34 -0800

    start newTimer and currentTimer Forms

---
+++ latest/frontend/javascript/components/Current-Timer.js
@@ -0,0 +1,9 @@
+import { AbstractTimer } from "./Abstract-Timer";
+import { NewTimerForm } from "./New-Timer-Form";
+
+export class CurrentTimer {
+    constructor(startingTimer){
+
+    }
+
+}
---
+++ latest/frontend/javascript/components/New-Timer-Form.js
@@ -0,0 +1,5 @@
+export class NewTimerForm {
+    constructor(){
+
+    }
+}
